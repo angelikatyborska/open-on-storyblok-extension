4de143128082ffb2c403ddfc05332c86382f38d3
Angelika Cathor <angelika.cathor@storyblok.com>
2025-02-17 16:54:08 +0100

    Initial implementation of finding a story

--- options/index.js
+++ options/index.js
@@ -1,20 +1,28 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
-    color: document.querySelector("#color").value,
+    space: {
+      name: document.querySelector("#space-name").value,
+      id: document.querySelector("#space-id").value,
+      accessToken: document.querySelector("#space-access-token").value,
+    }
   });
 }
 
 function restoreOptions() {
   function setCurrentChoice(result) {
-    document.querySelector("#color").value = result.color || "blue";
+    if (result?.space) {
+      document.querySelector("#space-id").value = result.space.id;
+      document.querySelector("#space-name").value = result.space.name;
+      document.querySelector("#space-access-token").value = result.space.accessToken;
+    }
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  let getting = browser.storage.sync.get("color");
+  let getting = browser.storage.sync.get("space");
   getting.then(setCurrentChoice, onError);
 }
 
--- popup/index.js
+++ popup/index.js
@@ -1,5 +1,42 @@
 let currentTabPromise = browser.tabs.query({active: true, currentWindow: true})
-currentTabPromise.then((tabs) => document.querySelector('#current-tab-url').textContent = tabs[0].url)
+currentTabPromise.then((tabs) => {
 
-let currentSettingPromise = browser.storage.sync.get("color");
-currentSettingPromise.then((result) => document.querySelector('#current-settings').textContent = result.color);
+  try {
+    const url = new URL(tabs[0].url);
+    const linkWrapper = document.querySelector('#story-link-wrapper')
+    linkWrapper.setAttribute('data-story-slug', url.pathname)
+    document.querySelector('#current-tab-slug').textContent = url.pathname
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+let currentSettingPromise = browser.storage.sync.get("space");
+
+currentSettingPromise.then((result) => {
+  document.querySelector('#space-id').textContent = result.space?.id;
+  document.querySelector('#space-name').textContent = result.space?.name;
+
+  if (result.space?.accessToken) {
+    const linkWrapper = document.querySelector('#story-link-wrapper')
+    const storySlug = linkWrapper.getAttribute('data-story-slug')
+    const apiRequestUrl = 'https://api.storyblok.com/v2/cdn/stories' + storySlug + '?token=' + result.space?.accessToken;
+
+    fetch(apiRequestUrl, { headers: { 'Accept': 'application/json' }}).then(response => {
+      if (response.status === 200) {
+          response.json().then(body => {
+            const link = document.createElement('a')
+            link.setAttribute('href', `https://app.storyblok.com/#/me/spaces/${result.space?.id}/stories/0/0/${body.story.id}`)
+            link.textContent = 'open story on Storyblok'
+            linkWrapper.textContent = ''
+            linkWrapper.appendChild(link)
+          }).catch(e => {
+            linkWrapper.textContent = 'error'
+            console.error('json error', e)
+          })
+      } else {
+        linkWrapper.textContent = 'Storyblok story not found'
+      }
+    }).catch(e => console.error('fetch error', e))
+  }
+});
